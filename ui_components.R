8567f0b358e7ec41f182ca3980b4144ed89c01f3
LukasW <125377952+LukasWillocx@users.noreply.github.com>
2025-12-29 23:36:33 +0100

    Reshuffled inputs

--- ui_components.R
+++ ui_components.R
@@ -3,28 +3,44 @@ toolbarUI <- function(id) {
   ns <- NS(id)
   
   div(class = "toolbar",
+      
+      div(class = "tool-group",
+          h4("Pick Color"),
+          div(
+            div(class = "color-tile", `data-color` = "#dc3545", `data-team` = "teamA", 
+                style = "background-color: #dc3545;", title = "Red"),
+            div(class = "color-tile", `data-color` = "#007bff", `data-team` = "teamB", 
+                style = "background-color: #007bff;", title = "Blue"),
+            div(class = "color-tile", `data-color` = "#28a745", `data-team` = "teamC", 
+                style = "background-color: #28a745;", title = "Green"),
+            div(class = "color-tile", `data-color` = "#ffc107", `data-team` = "teamD", 
+                style = "background-color: #ffc107;", title = "Yellow"),
+            div(class = "color-tile", `data-color` = "#6f42c1", `data-team` = "teamE", 
+                style = "background-color: #6f42c1;", title = "Purple"),
+            div(class = "color-tile", `data-color` = "#fd7e14", `data-team` = "teamF", 
+                style = "background-color: #fd7e14;", title = "Orange")
+          ),
+      ),
+      
       div(class = "tool-group",
           h4("Basic Tools"),
           div(
-            div(class = "tool-tile", `data-tool` = "number", title = "Place Numbers", "●"),
             div(class = "tool-tile", `data-tool` = "cross", title = "Add Cross", "✕"),
             div(class = "tool-tile", `data-tool` = "triangle", title = "Add Triangle", "▲"),
             div(class = "tool-tile", `data-tool` = "ball", title = "Place Basketball", "🏀"),
             div(class = "tool-tile", `data-tool` = "circle", title = "Draw Circle", "O")
           ),
       ),
-      
-      # Add 'players-group' class to this tool group for highlighting
-      div(class = "tool-group players-group",
-          h4("Players"),
-          
+
+      div(class = "tool-group",
+          h4("Place Players"),
           div(style = "margin-top: 10px;",
               div(
-                div(class = "number-tile", `data-number` = "1", "1"),
-                div(class = "number-tile", `data-number` = "2", "2"),
-                div(class = "number-tile", `data-number` = "3", "3"),
-                div(class = "number-tile", `data-number` = "4", "4"),
-                div(class = "number-tile", `data-number` = "5", "5")
+                div(class = "number-tile", `data-number` = "1", title = "Place Player 1", "1"),
+                div(class = "number-tile", `data-number` = "2", title = "Place Player 2", "2"),
+                div(class = "number-tile", `data-number` = "3", title = "Place Player 3", "3"),
+                div(class = "number-tile", `data-number` = "4", title = "Place Player 4", "4"),
+                div(class = "number-tile", `data-number` = "5", title = "Place Player 5", "5")
               )
           )
       ),
@@ -36,8 +52,8 @@ toolbarUI <- function(id) {
             div(class = "tool-tile", `data-tool` = "dotted-line", title = "Dotted Line", "┄")
           ),
           div(style = "margin-top: 10px;",
-              div(class = "tool-tile", `data-tool` = "line-with-stops", title = "Line with Stop", "┝"),
-              div(class = "tool-tile", `data-tool` = "dotted-line-with-stops", title = "Dotted Line with Stop", "┝-")
+              div(class = "tool-tile", `data-tool` = "line-with-stops", title = "Line with Stop", "┣"),
+              div(class = "tool-tile", `data-tool` = "dotted-line-with-stops", title = "Dotted Line with Stop", "┣-")
           )
       ),
       
@@ -65,23 +81,6 @@ toolbarUI <- function(id) {
           )
       ),
       
-      div(class = "tool-group",
-          h4("Pick Color"),
-          div(
-            div(class = "color-tile", `data-color` = "#dc3545", `data-team` = "teamA", 
-                style = "background-color: #dc3545;", title = "Red"),
-            div(class = "color-tile", `data-color` = "#007bff", `data-team` = "teamB", 
-                style = "background-color: #007bff;", title = "Blue"),
-            div(class = "color-tile", `data-color` = "#28a745", `data-team` = "teamC", 
-                style = "background-color: #28a745;", title = "Green"),
-            div(class = "color-tile", `data-color` = "#ffc107", `data-team` = "teamD", 
-                style = "background-color: #ffc107;", title = "Yellow"),
-            div(class = "color-tile", `data-color` = "#6f42c1", `data-team` = "teamE", 
-                style = "background-color: #6f42c1;", title = "Purple"),
-            div(class = "color-tile", `data-color` = "#fd7e14", `data-team` = "teamF", 
-                style = "background-color: #fd7e14;", title = "Orange")
-          )
-      )
   )
 }
 
@@ -110,7 +109,7 @@ canvasAreaUI <- function(id) {
       
       # Status Bar
       div(class = "status-bar",
-          span(id = "tool-status", "Ready - Select a tool and click on the court"),
+          span(id = "tool-status", ""),
           span(id = "coords", "")
       )
   )
